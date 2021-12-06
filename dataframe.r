3a75fbbab1ccce054e4727efcce22db9bde52d33
petracattano <80822620+petracattano@users.noreply.github.com>
2021-12-06 18:16:42 +0100

    Create dataframe.r

---
+++ dataframe.r
@@ -0,0 +1,8 @@
+# fine lezione 09/11/2021
+# dataframe = genere di dati rettangolari anche non omogenei (es colonne con integer, characters etc.
+# il mio primo dataframe
+dat <- data.frame( # funzione per creare dataframe
+id = 1:6, # prima colonna, sequenza di valori interi da 1 a 6
+name = c("Eokochia saxicola", "Cytisus aeolicus", "Anthemis aeolica", "Centaurea aeolica", "Silene hicesia", "Genista tyrrhena")# seconda colonna, nomi delle specie
+
+)
