a2704353be0a1acc829767a56e45aadb838426e2
petracattano <80822620+petracattano@users.noreply.github.com>
2022-01-04 18:52:19 +0100

    Update and rename 11_ciclo_for.r to 12_ciclo_for.r

--- 12_ciclo_for.r
+++ 12_ciclo_for.r
@@ -29,5 +29,25 @@ sr[i] <- sum(dune[i, ] > 0) #
 sr # per visualizzarlo unghezza 20
 # vettore di numeri interi con l
 # [1]  5 10 10 13 14 11 13 12 13 12  9  9 10  7  8  8  7  9  9  8
-sei a 34,30
+
+# funzione per calcolare la sr
+calculate_sr <- function(df) {
+sr <- vector() # come argomento passeremo un dataframe
+for (i in 1:nrow(dune)) {
+sr[i] <- sum(df[i, ] > 0) # df in modo che come argomento gli passiamo un datafame che quì si chiamerà df e farà i passaggi fatti con dune prima
+} # riprendiamo il ciclo e ivece di usare dune utilizziamo un segnaposto, alias, che sarà df che nel codice corrisponderà a dune
+return(sr)
+}
+calculate_sr(df = dune)
+# [1]  5 10 10 13 14 11 13 12 13 12  9  9 10  7  8  8  7  9  9  8 
+# si crea sempre un ciclo 
+
+# se voglio applicare la funzione ad un altro dataset
+data(mite)
+sr_mite <- calculate_sr(df = mite)
+sr_mite
+# [1] 20 23 19 23 19 21 16 21 16 19 25 19 18 23 18 17 14 18 21 20 ??? non è uguale
+
+
+
 
